745a2b1b804513bbc0fae5e69a09023b4a40971c
Argjend <argjend@coda.tech>
2020-05-20 23:06:51 +0200

    Home template

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class HomePage extends Component {
     render(){
         return (
-
+        <>
         <header>
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
                 <a className="navbar-brand" href="#">Carousel</a>
@@ -13,24 +13,75 @@ class HomePage extends Component {
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#">Link</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            <a className="nav-link disabled" to="#" tabIndex="-1" aria-disabled="true">Disabled</a>
                         </li>
                     </ul>
                     <form className="form-inline mt-2 mt-md-0">
-                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
                 </div>
             </nav>
             
         </header>
+            
+        <main role="main">
 
+            <div id="myCarousel" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src={window.location.origin + '/placeholder.svg'} style={{width:'100%', height:'100%', background:'#777', color:'#777'}} />
+                        <div className="container">
+                            <div className="carousel-caption text-left">
+                                <h1>Example headline.</h1>
+                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                                <p><a className="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                    {/* {% include icons/placeholder.svg width="100%" height="100%" background="#777" color="#777" text=" " title=" " %} */}
+                        <div className="container">
+                            <div className="carousel-caption">
+                                <h1>Another example headline.</h1>
+                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                                <p><a className="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                    {/* {% include icons/placeholder.svg width="100%" height="100%" background="#777" color="#777" text=" " title=" " %} */}
+                        <div className="container">
+                            <div className="carousel-caption text-right">
+                                <h1>One more for good measure.</h1>
+                                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                                <p><a className="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+        </main>
+        </>
         )
     }
 }
